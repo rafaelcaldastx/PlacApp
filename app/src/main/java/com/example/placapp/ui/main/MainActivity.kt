4b940c2cff922c3c8d85cfee851fe5ac510ab519
Logon Aluno <logonaluno@local.com>
2020-02-08 10:29:12 -0300

    Initial commit

---
+++ app/src/main/java/com/example/placapp/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.placapp.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.placapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
